cd716f2afb178485ace8ac6572fb6ff910522997
Dragomir-Alex <dragomario2k@gmail.com>
2022-11-21 08:39:20 +0200

    Commit

---
+++ TemaPractica1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TemaPractica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lim1, lim2, randNum;
+            var rand = new Random();
+
+            Console.WriteLine("Limita interval 1: ");
+            lim1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Limita interval 2: ");
+            lim2 = Convert.ToInt32(Console.ReadLine());
+
+            if (lim2 < lim1)
+                (lim1, lim2) = (lim2, lim1);
+            randNum = rand.Next(lim1, lim2 + 1);
+
+            Console.WriteLine("Ghiceste numarul din interval.");
+
+            while (true)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n == randNum)
+                {
+                    Console.WriteLine("Valoarea {0} este cea corecta!", n);
+                    break;
+                }
+                else if (n > randNum)
+                {
+                    Console.WriteLine("Valoarea {0} este prea mare.", n);
+                }
+                else
+                {
+                    Console.WriteLine("Valoarea {0} este prea mica.", n);
+                }
+            }
+        }
+    }
+}
